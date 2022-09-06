3c43c6b69a722fb62227651ff90431c90235865e
Yurii <vishtalyurii@gmail.com>
2022-09-06 09:11:17 +0200

    Treasure map excluding index

---
+++ main.py
@@ -0,0 +1,14 @@
+#treasure map
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+#23
+horizontal = int(position[0]) #1
+vertical = int(position[1])   #2
+selected_row = (map[vertical -1]) #excluding index
+selected_row[horizontal-1] = "X"
+print(f"{row1}\n{row2}\n{row3}")
